c341c88281b771a38fd4186b7ced00b71cac41c9
OttoBisno <ottobisno444@gmail.com>
2022-12-05 13:17:29 -0800

    Updated utils and staging for routes

---
+++ utils/auth.js
@@ -0,0 +1,10 @@
+// Redirects the user to the '/login' route if the user is not logged in
+const withAuth = (req, res, next) => {
+    if (!req.session.logged_in) {
+        res.redirect('/login');
+    } else {
+        next();
+    }
+};
+
+module.exports = withAuth;
