d117a325a697f86a5c1fd317ca8e52cbea98c2a7
OttoBisno <ottobisno444@gmail.com>
2022-12-05 17:19:02 -0800

    Completed logout and profile routes

---
+++ public/js/blog.js
@@ -0,0 +1,2 @@
+// Enable updating/deleting of blog posts that belong to the user
+// profile.js in mini-project has an example for how to delete already
---
+++ public/js/profile.js
@@ -0,0 +1,22 @@
+const newFormHandler = async (event) => {
+    event.preventDefault();
+
+    const title = document.querySelector('#blog-title').value.trim();
+    const body = document.querySelector('#blog-body').value.trim();
+    
+    if (title && body) {
+        const response = await fetch('/api/blogs', {
+            method: 'POST',
+            body: JSON.stringify({ title, body }),
+            headers: { 'Content-Type': 'application/json'}
+        });
+
+        if (response.ok) {
+            document.location.replace('/profile');
+        } else {
+            alert('Failed to create blog post');
+        }
+    }
+};
+
+document.querySelector('.new-blog-form').addEventListener('submit', newFormHandler);
